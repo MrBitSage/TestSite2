4e775fb5ad163eece8adb284baccecea7005ab6d
mrbitsage <mrbitsage@gmail.com>
2015-12-28 00:00:32 -0500

    got rid of warnings

---
+++ scripts/formScript.js
@@ -0,0 +1,13 @@
+function checkURL(form) {
+    var url = form.url_box.value;
+    alert(url);
+}
+
+function checkEmail(form) {
+    var email = form.email_box.value;
+    alert(email);
+}
+
+function submitForm(form) {
+
+}
